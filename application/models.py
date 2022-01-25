b55e518c198e4ab4775a29f7a73d87db01e068d9
Miguel Cemiller <miguelcemiller@gmail.com>
2022-01-25 16:13:11 +0800

    configured database

--- application/models.py
+++ application/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+
+from django.db.models.signals import pre_save, post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
+LOCATIONS = (
+    ('Alaminos', 'Alaminos'),
+    ('Bay', 'Bay'),
+    ('Biñan', 'Biñan'),
+    ('Cabuyao', 'Cabuyao'),
+    ('Calamba', 'Calamba'),
+    ('Calauan', 'Calauan'),
+    ('Cavinti', 'Cavinti'),
+    ('Famy', 'Famy'),
+    ('Kalayaan', 'Kalayaan'),
+    ('Liliw', 'Liliw'),
+    ('Los Baños', 'Los Baños'),
+    ('Luisiana', 'Luisiana'),
+    ('Lumban', 'Lumban'),
+    ('Mabitac', 'Mabitac'),
+    ('Magdalena', 'Magdalena'),
+    ('Majayjay', 'Majayjay'),
+    ('Nagcarlan', 'Nagcarlan'),
+    ('Paete', 'Paete'),
+    ('Pagsanjan', 'Pagsanjan'),
+    ('Pakil', 'Pakil'),
+    ('Pangil', 'Pangil'),
+    ('Pila', 'Pila'),
+    ('Rizal', 'Rizal'),
+    ('San Pablo', 'San Pablo'),
+    ('San Pedro', 'San Pedro'),
+    ('Santa Cruz', 'Santa Cruz'),
+    ('Santa Maria', 'Santa Maria'),
+    ('Santa Rosa', 'Santa Rosa'),
+    ('Siniloan', 'Siniloan'),
+    ('Victoria', 'Victoria'),
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    user_image = models.ImageField(null=True, blank=True, upload_to='static/images/users/')
+
+    dog_name = models.CharField(max_length=200, blank=True, null=True)
+    dog_age = models.IntegerField(max_length=10, blank=True, null=True)
+    dog_description = models.TextField(blank=True, null=True)
+    dog_image = models.ImageField(null=True, blank=True, upload_to='static/images/dogs/')
+
+    location = models.CharField(max_length=20, choices=LOCATIONS, blank=False, null=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
+
+# Create Profile when User is created
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user = user,
+        )
+    print("this is fired off")
+
+# Delete User when Profile is deleted
+@receiver(post_delete, sender=Profile)
+def delete_student(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
--- application/views.py
+++ application/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from . models import Profile
+
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'application/home.html')
+    profiles = Profile.objects.all().first()
+
+    context = {'profiles': profiles}
+
+    print(profiles.dog_image)
+
+    return render(request, 'application/home.html', context)
